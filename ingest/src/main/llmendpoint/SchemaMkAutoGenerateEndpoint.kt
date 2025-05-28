2cdcf48cbbb0c1936d07fdf84a8a698d9a334ed7
junheyoungpark <gitpdeathwa@yonsei.ac.kr>
2025-05-29 08:03:54 +0900

    impl schema markdown auto generate logic

---
+++ ingest/src/main/llmendpoint/SchemaMkAutoGenerateEndpoint.kt
@@ -0,0 +1,12 @@
+package io.ybigta.text2sql.ingest.llmendpoint
+
+import io.ybigta.text2sql.ingest.logic.schema_ingest.TableSchema
+import kotlinx.serialization.json.JsonObject
+
+interface SchemaMkAutoGenerateEndpoint {
+    /**
+     * @param tableSchmea: Serialized json of [TableSchema]
+     * @return markdown document about table schema
+     */
+    fun request(tableSchmea: JsonObject): String
+}
