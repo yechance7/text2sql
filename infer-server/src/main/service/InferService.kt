3ff2daaa3cce21b8f6e6c05b9e7d01568b80eba1
junheyoungpark <gitpdeathwa@yonsei.ac.kr>
2025-08-03 07:38:22 +0900

    removed unnecessary coroutineScope

--- infer-server/src/main/service/InferService.kt
+++ infer-server/src/main/service/InferService.kt
@@ -5,21 +5,19 @@ import io.ybigta.text2sql.infer.core.Inferer
 import io.ybigta.text2sql.infer.core.Question
 import io.ybigta.text2sql.infer.core.config.InferConfig
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 
 internal class InferService(
     private val inferer: Inferer,
     private val inferConfig: InferConfig,
     private val scope: CoroutineScope
 ) {
-    suspend fun infer(question: String): InferResp = coroutineScope {
+    suspend fun infer(question: String): InferResp {
         val q = Question.fromConfig(question, inferConfig, scope)
 
         val inferResult = inferer.infer(q)
 
         val inferResp = InferResp.from(inferResult)
 
-        return@coroutineScope inferResp
+        return inferResp
     }
 }
